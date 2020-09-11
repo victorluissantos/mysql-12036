e225d75749d0aa8a6c81b0ec2e5786158cc2a8b9
Victor L. Santos <victorluissantos@live.com>
2020-09-10 21:53:48 -0300

    upper with exercicios nivel 2

--- aula7/exercicios.sql
+++ aula7/exercicios.sql
@@ -60,42 +60,64 @@ WHERE
 
 
 /** CRIANDO VIEWS */
-/*
-1 - Qual o usuario que mais cria eventos ?
-2 - crear uma view `usuario_compromissos` que retorne lista de usuario com sua lista de compromissos
-| usuario | compromissos |
-  Heitor  |  1, 2, 4, 5  |
+-- 1 - Qual o usuario que mais cria eventos ?
+SELECT 
+    u.*,
+    (SELECT 
+            COUNT(c.id)
+        FROM
+            compromissos c
+        WHERE
+            c.usuario_id = u.id) AS `total_evento`
+FROM
+    usuarios u
+ORDER BY 8 DESC
+LIMIT 1;
 
-3 - Criar uma view `ranking_compromissos` que retorne um ranking de usuarios por compromissos
-|	nome	| total |
-|   Julio	| 12	|
-|   Diego	| 8		|
+-- 2 - crear uma view `usuario_compromissos` que retorne lista de usuario com sua lista de compromissos
+--| usuario | compromissos |
+--  Heitor  |  1, 2, 4, 5  |
+CREATE VIEW usuario_compromissos AS
+    (SELECT 
+        u.nome AS `usuario`,
+        (SELECT 
+                GROUP_CONCAT(c.id)
+            FROM
+                compromissos c
+            WHERE
+                c.usuario_id = u.id) AS `compromissos`
+    FROM
+        usuarios u);
 
-4 - Criar uma view `ranking_marcacoes` que retorne um ranking de usuarios por marcaoes em compromissos
-|	nome	| marcacoes |
-|   Victor	| 3			|
-|   Miake	| 2			|
+-- 3 - Criar uma view `ranking_compromissos` que retorne um ranking de usuarios por compromissos
+--|	nome	| total |
+--|   Julio	| 12	|
+--|   Diego	| 8		|
 
-5 - Criar uma view `ranking_marcacoes` que retorne um ranking de usuarios por compromissos com marcacoes
-|	nome	| marcacoes |
-|   Maicon	| 3			|
-|   Miake	| 2			|
+-- 4 - Criar uma view `ranking_marcacoes` que retorne um ranking de usuarios por marcaoes em compromissos
+--|	nome	| marcacoes |
+--|   Victor	| 3			|
+--|   Miake	| 2			|
 
-6 - Criar uma view que retorne
-|usuario	|	nome	|	data				|	marcacoes		|
-|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|
-|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|
-OBS: deve ser ordenada pela data, e a data estara em padrao pt_bt sem o segundos
+-- 5 - Criar uma view `ranking_marcacoes` que retorne um ranking de usuarios por compromissos com marcacoes
+--|	nome	| marcacoes |
+--|   Maicon	| 3			|
+--|   Miake	| 2			|
 
-7 - Criar uma view que retorne
-|usuario	|	nome	|	data				|	marcacoes		| qtd_convidados |
-|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|		2		 |
-|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|		1		 |
-OBS: deve ser ordenada pela qt_convidados, e a data estara em padrao pt_bt sem o segundos
+-- 6 - Criar uma view que retorne
+--|usuario	|	nome	|	data				|	marcacoes		|
+--|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|
+--|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|
+--OBS: deve ser ordenada pela data, e a data estara em padrao pt_bt sem o segundos
 
-8 - Criar uma view que retorne
-|usuario	|	nome	|	data				|	marcacoes		| qtd_convidados | lidos 	| confirmados 	| justificado
-|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|		2		 |	2		|	  1			|   1
-|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|		1		 |	1		|		0		|
-OBS: deve ser ordenada pela qt_convidados, e a data estara em padrao pt_bt sem o segundos, se o compromisso estiver como confirmado ele tambem soma no lido
-*/
+-- 7 - Criar uma view que retorne
+--|usuario	|	nome	|	data				|	marcacoes		| qtd_convidados |
+--|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|		2		 |
+--|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|		1		 |
+--OBS: deve ser ordenada pela qt_convidados, e a data estara em padrao pt_bt sem o segundos
+
+-- 8 - Criar uma view que retorne
+--|usuario	|	nome	|	data				|	marcacoes		| qtd_convidados | lidos 	| confirmados 	| justificado
+--|	1002	|	Diego	|	20/03/2020 23:12	|	Heitor, Mikaele	|		2		 |	2		|	  1			|   1
+--|	1004	|	Maicon	|	23/03/2020 23:12	|	Camila			|		1		 |	1		|		0		|
+--OBS: deve ser ordenada pela qt_convidados, e a data estara em padrao pt_bt sem o segundos, se o compromisso estiver como confirmado ele tambem soma no lido
