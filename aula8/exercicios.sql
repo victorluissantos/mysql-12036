a7de40d3a1072254ff3aaa13ec37847cbddb7f7c
Victor L. Santos <victorluissantos@live.com>
2020-09-11 21:57:37 -0300

    upper with last exercicios

---
+++ aula8/exercicios.sql
@@ -0,0 +1,8 @@
+/*
+ -- 1 Qual o usuario que mais leu compromisso e nao justificou
+ -- 2 Qual o usuario que mais leu compromisso e faltou
+ -- 3 Qual o tempo media em horas de cada usuario, entre a marcacao e alguma acao na marcaco
+ -- 4 Qual a quantidade de compromissos por mes no ano de 2020 para cada usuario
+ -- 5 Qual o compromisso de maior perioro
+ -- 6 Qual o compromisso com maior quantidade de recorrencia
+*/
